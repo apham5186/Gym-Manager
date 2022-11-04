f7b16fa889c858f51c9374572746d3977628bc8b
apham5186 <66178797+apham5186@users.noreply.github.com>
2022-11-04 18:42:59 -0400

    Add files via upload

--- Project 3/src/src/GymManagerController.java
+++ Project 3/src/src/GymManagerController.java
@@ -3,10 +3,7 @@ import java.io.File;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -16,4 +13,15 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.scene.control.Alert.AlertType;
 
 public class GymManagerController {
+    @FXML
+    private Button addMember, removeMember, checkIn, drop;
+
+    @FXML
+    private TextField firstName, lastName, DOB, className, instructor, location;
+
+    @FXML
+    private TextArea messageArea;
+
+    @FXML
+   void
 }
